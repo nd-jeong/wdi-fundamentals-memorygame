37d14ff34277b9b1862433c8f5ee4eaec30eb83c
Andy Jeong <aj@Andys-Air.fios-router.home>
2019-04-14 20:15:33 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped over " + cardOne);
+console.log("User flipped over " + cardThree);
