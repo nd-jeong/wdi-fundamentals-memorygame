ee52b8b3de567a2392f887e35342484627390c37
Andy Jeong <aj@Andys-Air.fios-router.home>
2019-04-18 01:30:36 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,20 +24,33 @@ var cards = [
 var cardsInPlay = [];
 
 function checkForMatch() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+  this.setAttribute('src', (cards.cardImage));
+  if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
 };
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
+  // console.log(cards[cardId]);
   cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
   console.log(cards[cardId].suit);
   console.log(cards[cardId].cardImage);
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+  for (i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    var gameBoard = document.getElementById('game-board');
+    gameBoard.appendChild(cardElement);
+  }
+};
+
+createBoard();
